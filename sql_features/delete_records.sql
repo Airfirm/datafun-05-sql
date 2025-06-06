8725ba38033bc64569e03d31fa4de4774ba20d5b
Oluwafemi Salawu <ogsalawu@gmail.com>
2025-06-06 16:04:24 -0500

    Update delete and update file

--- sql_features/delete_records.sql
+++ sql_features/delete_records.sql
@@ -1,2 +1,22 @@
+-- Delete author only if they have this specific book
 DELETE FROM authors
-WHERE name = 'Oluwafemi Salawu';
+--WHERE author_id = 'AUTHOR_016'
+WHERE author_id = 'AUTHOR_011'
+AND EXISTS (
+    SELECT 1 FROM books 
+    WHERE books.author_id = authors.author_id
+    --AND book_id = 'BOOK_027';
+    AND book_id = 'BOOK_020'
+);
+
+-- Then delete the book
+DELETE FROM books 
+--WHERE book_id = 'BOOK_027';
+WHERE book_id = 'BOOK_020';
+
+
+--DELETE authors, books
+--FROM authors
+--INNER JOIN books ON authors.author_id = books.author_id
+--WHERE authors.author_id = 'AUTHOR_16'
+--AND books.book_id = 'BOOK_27';
